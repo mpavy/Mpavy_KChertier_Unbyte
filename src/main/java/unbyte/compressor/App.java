991bdfd7945ce2a6aee57604df8a1b27844bac49
Myriam Pavy <myriam.pavy@audencia.com>
2020-02-28 00:07:03 +0100

    Adding command line interface (picocli)

--- src/main/java/unbyte/compressor/App.java
+++ src/main/java/unbyte/compressor/App.java
@@ -1,47 +1,97 @@
 package unbyte.compressor;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.BitSet;
+import picocli.CommandLine;
+
+import java.io.*;
+import java.util.concurrent.Callable;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App implements Callable<Integer>
 {
+
+	@CommandLine.Option(names = {"-i", "--input"}, description = "Enter the input file path")
+    private
+    String path;
+
+	@CommandLine.Option(names = "-out", description = "Enter the output file name")
+    String outputFileName;
+
+	@CommandLine.Option(names = "-algo", description = "The algorithm you want to use : Huffman (default) or LZW")
+	private
+	String algorithm;
+
+	@CommandLine.Option(names = "--compress", description ="Compress the file (default)")
+	private
+	boolean compression;
+
+	@CommandLine.Option(names = "--decompress", description ="Decompress the file")
+	private
+	boolean decompression;
+
+	@CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "Display a help message")
+	private boolean helpRequested = false;
+
 	public static void main( String[] args ) throws IOException
 	{
-
-		try {
-			InputStream inputStream = new FileInputStream("testToCompress.txt");
-			InputStream stream = new BufferedInputStream(inputStream);
-			
-//			Compressor man = new HuffmanCompressor();
-//			man.compress(stream,"result");
-//			man.decompress(stream,"res");
-			
-			Compressor lzw = new LZWCompressor();
-			lzw.compress(stream, "test");
-			
-			
-			InputStream toDecompress = new FileInputStream("test.lzw");
-			InputStream streamDecompress = new BufferedInputStream(toDecompress);
-			
-			lzw.decompress(streamDecompress, "uncompressed");
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block FileNotFoundException
-			e.printStackTrace();
-		}
-	     
-	    
-		//System.out.println(man.translate("A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"));
-
-		
+		int exitCode = new CommandLine(new App()).execute(args);
+		System.exit(exitCode);
 	}
+
+	@Override
+	public Integer call() throws IOException {
+		Compressor man;
+		if (algorithm == null)
+		{
+			System.out.println("No available algorithm was indicated, so Huffman is picked by default");
+			man = new HuffmanCompressor();
+		} else {
+			man = getAlgorithm(algorithm);
+		}
+
+		InputStream stream;
+		if (path == null)
+		{
+			System.out.println("Please provide an input path");
+			return 1;
+		} else {
+			stream = getInput(path);
+		}
+
+		if (outputFileName == null){
+			System.out.println("No output file name was provided. Default is 'result'");
+			outputFileName = "result";
+		}
+
+        if (compressionIsChosen(compression, decompression))
+		{
+			man.compress(stream, outputFileName);
+			return 0;
+		}
+		man.decompress(stream,outputFileName);
+        return 0;
+	}
+
+    public Compressor getAlgorithm(String algo){
+	    if (algo.equals("Huffman"))
+	        return new HuffmanCompressor();
+	    if (algo.equals("LZW"))
+	        return new LZWCompressor();
+        System.out.println("No available algorithm was indicated, so Huffman is picked by default");
+        return new HuffmanCompressor();
+        }
+
+    public InputStream getInput(String path) throws FileNotFoundException{
+        InputStream inputStream = new FileInputStream(path);
+        return new BufferedInputStream(inputStream);
+    }
+
+    public boolean compressionIsChosen(boolean compression, boolean decompression){
+		if (compression == decompression)
+			return true;
+		return compression;
+	}
+
 }
